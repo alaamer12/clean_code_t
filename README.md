03bfe21c2c4e2d1d6dbd8a7f477dd7aa74dfc84d
alaamer12 <ahmedmuhmmed239@gmail.com>
2024-05-22 00:15:08 +0300

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+# About
+this project is a practice on the book clean code by Uncle Bob(https://leanpub.com/clean-code-2)
---
+++ main.py
@@ -0,0 +1,11 @@
+class Avoid:
+    pass
+class Do:
+    pass
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    avoid = Avoid()
+    do = Do()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
